4092c0ea0f3eb4ad5481d641bb1cf8130956b818
MichaelRayven <themikerayven@gmail.com>
2022-06-11 22:55:52 +0700

    Added logic to update, like, delete post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,11 +1,11 @@
 import * as api from '../api'
+import { FETCH_POSTS, CREATE_POST, UPDATE_POST, DELETE_POST, LIKE_POST } from '../constants/postsActionTypes'
 
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        const action = { type: 'FETCH_ALL', payload: data }
 
-        dispatch(action)
+        dispatch({ type: FETCH_POSTS, payload: data });
     } catch (error) {
         console.error(error.message)
     }
@@ -13,13 +13,40 @@ export const getPosts = () => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
-        post.tags = post.tags.split(' ')
         const { data } = await api.createPost(post)
-        const action = { type: 'CREATE', payload: data }
         
-        dispatch(action)
+        dispatch({ type: CREATE_POST, payload: data })
     } catch (error) {
         console.error(error.message)
     }
 }
 
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post)
+        
+        dispatch({ type: UPDATE_POST, payload: data })
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id)
+        
+        dispatch({ type: DELETE_POST, payload: id })
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id)
+        
+        dispatch({ type: LIKE_POST, payload: data })
+    } catch (error) {
+        console.error(error.message)
+    }
+}
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,17 @@
+import { FETCH_POSTS, CREATE_POST, UPDATE_POST, DELETE_POST, LIKE_POST } from '../constants/postsActionTypes'
+
 const reducer = (posts = [], action) => {
     switch(action.type) {
-        case "CREATE": 
+        case CREATE_POST: 
             return [...posts, action.payload]
-        case "FETCH_ALL": 
+        case FETCH_POSTS: 
             return action.payload
+        case UPDATE_POST:
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post)
+        case DELETE_POST:
+            return posts.filter((post) => post._id !== action.payload)
+        case LIKE_POST:
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post)
         default:
             return posts
     }
