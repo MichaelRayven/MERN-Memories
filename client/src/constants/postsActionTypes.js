0a817a20a98ffbd04d81c5ac65ac73c00d1ee7a2
MichaelRayven <themikerayven@gmail.com>
2022-06-11 21:39:18 +0700

    Added action type constants for posts reducer

---
+++ client/src/constants/postsActionTypes.js
@@ -0,0 +1,5 @@
+export const FETCH_POSTS = "FETCH_ALL"
+export const CREATE_POST = "CREATE"
+export const UPDATE_POST = "UPDATE"
+export const DELETE_POST = "DELETE"
+export const LIKE_POST = "LIKE"
