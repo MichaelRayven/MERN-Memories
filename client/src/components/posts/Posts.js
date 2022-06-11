b84157eb8968821efa8b19e205833db16df7dd6d
MichaelRayven <themikerayven@gmail.com>
2022-06-11 22:56:43 +0700

    Defined posts component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { CircularProgress, Grid, Box } from '@mui/material'
+import { useSelector } from 'react-redux'
 import Post from './post/Post'
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
+    const posts = useSelector((state) => state.posts)
+
     return (
-        <>
-            <h1>Posts</h1>
-            <Post></Post>
-            <Post></Post>
-        </>
+        !posts.length ?  <Box sx={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center' }} ><CircularProgress/></Box>: (
+            <Grid sx={{display: 'flex'}} container alignItems="stretch" spacing={3}>
+                {posts.map(post => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} setCurrentId={setCurrentId} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
